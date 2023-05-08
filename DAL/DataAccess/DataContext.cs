7cc565d72e99360dd669ab8a86c0b5e48b6db712
Azset <Azset@DESKTOP-77DHT7I>
2023-05-08 13:57:20 +0400

    Add project files.

---
+++ DAL/DataAccess/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PustokProject.Models;
+
+namespace PustokProject.DAL.DataAccess
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+        public DbSet<Tag>Tags { get; set; } 
+        public DbSet<Genre>Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book>Books { get; set; }
+        public DbSet<BookImage> BookImages { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using PustokProject.DAL.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlServer(@"Server=LAPTOP-VID038BJ\SQLEXPRESS;Database=PustokProject;Trusted_Connection=true");
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
